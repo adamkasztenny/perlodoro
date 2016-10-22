5e35672b646800f50b57ff00bc63d1842d63ca81
Adam Kasztenny <adamkasztenny@gmail.com>
2016-10-22 11:16:15 -0400

    Progress, but not there yet

--- perladoro.pl
+++ perladoro.pl
@@ -3,18 +3,28 @@
 use strict;
 use warnings;
 
-use Time::HiRes;
+use Time::HiRes qw (setitimer ITIMER_REAL clock);
 use Term::ANSIColor;
+use POSIX qw(pause);
 
-user constant POMO_TIME => 25 * 60;
+use constant POMO_TIME => 25 * 60;
+my $elapsed = 0;
+my $start = clock();
 
-my $infinite = 0;
-my $signal;
+clear_screen();
 
-if ($ARGV[0] eq '-i') {
-    $infinite = 1;
+# thanks to https://www.febo.com/pages/perl_alarm_code/
+$SIG{ALRM} = sub { print $start - clock(), "\n" };
+setitimer(ITIMER_REAL, 1, 1);
+
+while ($elapsed < POMO_TIME) {
+    pause;
+    clear_screen();
+    $elapsed++;
 }
 
-do {
-    Time::HiRes->settimer($signal, POMO_TIME);
-} while ($infinite)
+# thanks to http://stackoverflow.com/questions/197933/whats-the-best-way-to-clear-the-screen-in-perl
+sub clear_screen {
+    print "\033[2J";
+    print "\033[0;0H";
+}
